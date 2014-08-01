5d7e2320bfe8b5b5aaf4dc5f33fc26474bec354b
ridolfil <ridolfi.luc@gmail.com>
2014-08-01 20:12:42 +0100

    First Commit

---
+++ MarketInsights/js/data.js
@@ -0,0 +1,175 @@
+// JavaScript source code
+
+
+
+var chart_data = [
+
+    {
+        year: 2000,
+        value: 1567
+    },
+    {
+        year: 2001,
+        value: 1665
+    },
+    {
+        year: 2002,
+        value: 1568
+    },
+    {
+        year: 2003,
+        value: 1092
+    },
+    {
+        year: 2004,
+        value: 1143
+    },
+    {
+        year: 2005,
+        value: 1325
+    },
+    {
+        year: 2006,
+        value: 1856
+    },
+    {
+        year: 2007,
+        value: 1293
+    }
+];
+
+var test_data = [
+                     {
+                         value: 12
+                     },
+                     {
+                         value: 13
+                     },
+                 {
+                     value: 16
+                 }
+];
+var market_data = [
+    {
+        name: "S&P500",
+        ind_value: 1977.65,
+        change: 3.2,
+        high: 1984.22,
+        low: 1975.43
+    },
+    {
+        name: "FTSE100",
+        ind_value: "60.01",
+        change: 2.3,
+        high: 62.5,
+        low: 58.6
+    },
+    {
+        name: "Nasdaq",
+        ind_value: 4380,
+        change: 1.67,
+        high: 4385,
+        low: 4371
+    },
+    {
+        name: "Dow Jones",
+        ind_value: 16668,
+        change: 1.23,
+        high: 16674,
+        low: 16659
+    }
+];
+
+var U = [
+            {
+                "country": "United States",
+                "year": "1994",
+                "value": 4.9
+            },
+            {
+                "country": "United States",
+                "year": "1995",
+                "value": 9.2
+            },
+            {
+                "country": "United States",
+                "year": "1996",
+                "value": 16.4
+            },
+            {
+                "country": "United States",
+                "year": "1997",
+                "value": 21.6
+            },
+            {
+                "country": "United States",
+                "year": "1998",
+                "value": 30.1
+            },
+            {
+                "country": "United States",
+                "year": "1999",
+                "value": 35.9
+            },
+            {
+                "country": "United States",
+                "year": "2000",
+                "value": 43.1
+            },
+            {
+                "country": "United States",
+                "year": "2001",
+                "value": 49.2
+            },
+            {
+                "country": "United States",
+                "year": "2002",
+                "value": 59.0
+            },
+            {
+                "country": "United States",
+                "year": "2003",
+                "value": 61.9
+            },
+            {
+                "country": "United States",
+                "year": "2004",
+                "value": 65
+            },
+            {
+                "country": "United States",
+                "year": "2005",
+                "value": 68.3
+            },
+            {
+                "country": "United States",
+                "year": "2006",
+                "value": 69.2
+            },
+            {
+                "country": "United States",
+                "year": "2007",
+                "value": 75.3
+            },
+            {
+                "country": "United States",
+                "year": "2008",
+                "value": 74.2
+            },
+            {
+                "country": "United States",
+                "year": "2009",
+                "value": 71.2
+            },
+            {
+                "country": "United States",
+                "year": "2010",
+                "value": 74.2
+            },
+            {
+                "country": "United States",
+                "year": "2011",
+                "value": 78.2
+            }
+];
+
